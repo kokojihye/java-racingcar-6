057558c8a530995c1a5de2c1868fa34f3479a077
kokojihye <jihyevivianako@gmail.com>
2023-10-28 23:59:37 +0900

    feat : define const to print const essage

---
+++ src/main/java/racingcar/constant/MessageConst.java
@@ -0,0 +1,9 @@
+package racingcar.constant;
+
+public final class MessageConst {
+    public static final String GET_CAR_NAME_MSG = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String GET_ATTEMPT_COUNT_MSG = "시도할 회수는 몇회인가요?";
+    public static final String REPORT_RESULT_MSG = "실행 결과";
+    public static final String REPORT_WINNER_MSG = "최종 우승자";
+    public static final String CAR_NAME_EXCEPTION_MSG = "자동차 이름은 5자 이하만 가능합니다.";
+}
